e4649480c84869d7ac67c54e5967399b30e65e14
David Chapman <dchapman@fastcompany.com>
2012-08-02 21:17:48 -0400

    mostly complete with version 3

--- js/script.js
+++ js/script.js
@@ -4,14 +4,25 @@ http://www.thisischapman.com
 */
 
 $(document).ready(function() {
-	$('.fit-text').fitText();
-	
+    // Initiate responsive slider
+    if($('body').hasClass('home')) {
+          $("#slideshow ul").responsiveSlides({
+              maxwidth: 420,
+              speed: 800,
+              timeout: 3500
+          });
+    } else {}
+
+    // Initiate responsive text
+    $('.fit-text').fitText();
+
+    // Initiate shop/buy botton animation
 	buyButton = $('.buy-button');
 	currentSize = buyButton.css('font-size');
-	
+
 	$('.buy-button').hover(function() {
 		currentSize = buyButton.css('fontSize');
-		
+
 		$(this).stop().animate({
 			fontSize: 1.2 * parseInt(currentSize)
 		}, 200);
